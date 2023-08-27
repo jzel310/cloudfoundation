02a204f059b0293c6803ccef6b147257aaa1af36
jzelada-ww <jzelada@wwnorton.com>
2023-08-27 11:53:12 -0600

    chore: added action refactoring

--- sandbox-aks/backend.tf
+++ sandbox-aks/backend.tf
@@ -1,10 +1,11 @@
 
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.67.0"
-    }
+  required_version = ">= 1.3"
+  # required_providers {
+  #   azurerm = {
+  #     source  = "hashicorp/azurerm"
+  #     version = "=3.67.0"
+  #   }
   }
 
   backend "azurerm" {
